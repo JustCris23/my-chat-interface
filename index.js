66389d25d8f7dd07827ce9876419f8e0498337eb
JustCris23 <129999174+JustCris23@users.noreply.github.com>
2023-04-21 10:54:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next(); });
+
+let currentChatId = null;
 
 app.post('/send-message', async (req, res) => {
   const message = req.body.message;
-  const chatId = req.body.chatId;
   const botToken = '6094204864:AAEUxSO-L1WcnOxKTil51xHDytDU1OOKQIg';
 
+  if (!currentChatId) {
+    res.status(400).json({ error: 'No chat ID available' });
+    return;
+  }
+
   try {
-    const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`);
+    const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${currentChatId}&text=${encodeURIComponent(message)}`);
     const data = await response.json();
     res.json(data);
   } catch (error) {
@@ -21,7 +28,16 @@ app.post('/send-message', async (req, res) => {
   }
 });
 
-// Add this new route to serve the chat_interface.html file
+app.post('/webhook', (req, res) => {
+  const data = req.body;
+
+  if (data.message && data.message.chat && data.message.chat.id) {
+    currentChatId = data.message.chat.id;
+  }
+
+  res.sendStatus(200);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'chat_interface.html'));
 });
